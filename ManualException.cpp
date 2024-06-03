0d0c5d7e551b214448f0409e9d58be050e8c0295
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2024-06-03 15:08:40 +0700

    ManualException selesai

--- ManualException.cpp
+++ ManualException.cpp
@@ -2,3 +2,15 @@
 using namespace std;
 
 int main() {
+    try {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch (int a) {
+        cout << "Pengecualian akan dieksekusi" << endl;
+    }
+    catch (...) {
+        cout << "default pengecualian dieksekusi" << endl;
+    }
+    return 0;
+}
