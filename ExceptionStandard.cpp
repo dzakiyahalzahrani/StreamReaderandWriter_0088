c5edc050485ca5c6125fa79749b87dded7f1ae84
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2024-06-03 15:18:53 +0700

    ExceptionStandard selesai

--- ExceptionStandard.cpp
+++ ExceptionStandard.cpp
@@ -5,6 +5,15 @@ using namespace std;
 
 int main()
 {
+        cout << "Awal Program" << endl; //penanda 1
+    try {
+        array<int, 3> data = {8, 4, 2};
+        cout << data.at(5) << endl;
+    }
+    catch (exception& e) {
+        cout << e.what() << endl;
+    }
+    cout << "Baris Program Yang Terakhir" << endl;
 
-
+    return 0;
 }
