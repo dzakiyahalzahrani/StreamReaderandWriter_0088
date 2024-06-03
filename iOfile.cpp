7e231ab24e23176a0ead9c407bac3ce9efb0c9ee
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2024-06-03 14:45:16 +0700

    selesai dlm menulis dan menutup file

--- iOfile.cpp
+++ iOfile.cpp
@@ -16,4 +16,14 @@ int main(){
      //unlimited loop unk menulis
      while (true) {
         cout << "- ";
-       
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukan karakter q
+        if(baris == "q") break;
+        //menulis dan memasukan nilai dari 'baris'ke dalam file
+        outfile << baris << endl;
+     }
+     //selesai dalam menulis sekarang tutup file nya
+     outfile.close();
+
+    
